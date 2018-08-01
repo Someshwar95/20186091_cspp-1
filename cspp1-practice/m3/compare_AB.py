dfba939cb0aa18e2dcd55f9844dcca7be7936959
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-01 18:49:27 +0530

    Comparison

---
+++ cspp1-practice/m3/compare_AB.py
@@ -0,0 +1,14 @@
+'''
+#varA&varB: Someshwar95
+Comparison program
+'''
+VARA = 23
+VARB = 40
+if type(VARA) == str or type(VARB) == str:
+    print("bigger")
+if VARA > VARB:
+    print("bigger")
+if VARA == VARB:
+    print("equal")
+if VARA < VARB:
+    print("smaller")
