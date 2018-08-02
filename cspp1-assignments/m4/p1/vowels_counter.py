bc18ab5601efbad1741dbf36f6b02755d415232a
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-02 18:49:06 +0530

    " -> 0 of 4 passed. pylint: 4.44/10 "

--- cspp1-assignments/m4/p1/vowels_counter.py
+++ cspp1-assignments/m4/p1/vowels_counter.py
@@ -12,11 +12,11 @@ def main():
     vowels contained in the string s
     '''
     s_str = input()
-    vowels_ow = 0
+    vowels_ow = 0    
     for x_lm in s_str:
         if x_lm == 'a' or x_lm == 'e' or x_lm == 'i' or x_lm == 'o' or x_lm == 'u':
             vowels_ow += 1
     print('vowels_ow are: ' + str(vowels_ow))
 
-if __name__ == "__main__":
+    if __name__ == "__main__":
         main()
