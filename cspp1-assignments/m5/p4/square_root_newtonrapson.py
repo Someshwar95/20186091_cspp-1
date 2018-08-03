b12604af4668ca9dfb25481d29637ac3d44dae62
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-03 20:09:19 +0530

    " -> 3 of 3 passed. pylint: 7.78/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -9,16 +9,16 @@
 # output: 6.999999999999991
 '''
 def main():
-    s = float(input())
     '''
     # epsilon and step are initialized
     # don't change these values
     '''
+    s = float(input())
     epsilon = 0.01
     guess = s/2.0
     while abs(guess*guess - s) >= epsilon:
         guess = guess - (((guess**2) - s)/(2*guess))
     print(guess)   
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
