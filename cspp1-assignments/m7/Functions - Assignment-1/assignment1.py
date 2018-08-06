133b30ccea5ab01f065abe58d2035f13efa60072
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-06 22:24:21 +0530

    " -> 10 of 10 passed. pylint: -3.85/10 "

--- cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/Functions - Assignment-1/assignment1.py
@@ -1,15 +1,12 @@
+'''
 # Functions | Assignment-1 - Paying Debt off in a Year
-
 # Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the
 # credit card company each month.
-
 # The following variables contain values as described below:
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
 # monthlyPaymentRate - minimum monthly payment rate as a decimal
-
 # For each month, calculate statements on the monthly payment and remaining balance. At the end of 12 months, print out the remaining
-'''
 # balance. Be sure to print out no more than two decimal digits of accuracy - so print
 # Remaining balance: 813.41
 # instead of
@@ -37,6 +34,6 @@ def main():
     data = list(map(float, data))
     print('Remaining balance:',payingDebtOffInAYear(data[0],data[1],data[2]))
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
 
