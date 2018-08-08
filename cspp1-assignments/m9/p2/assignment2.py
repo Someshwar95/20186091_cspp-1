46188d74b961856be91325a01336b85e796da46d
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-08 19:02:13 +0530

    " -> 1 of 9 passed. pylint: 7.89/10 "

--- cspp1-assignments/m9/p2/assignment2.py
+++ cspp1-assignments/m9/p2/assignment2.py
@@ -14,12 +14,12 @@ def get_guessed_word(secret_word, letters_guessed):
       what letters in secret_word have been guessed so far.
     '''
     # FILL IN YOUR CODE HERE...
-    secret_word = ""
-    for char in letters_guessed:
-    	if char in secret_word:
-    		secret_word += '_'
+    str = ""
+    for char in secret_word:
+    	if char in letters_guessed:
+    		str += '_'
 
-    return secret_word
+    return str
     
 
 def main():
