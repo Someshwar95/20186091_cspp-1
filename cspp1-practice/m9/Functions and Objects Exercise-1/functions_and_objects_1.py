e81d3acce33ca9aadf4046118968d0698ece8c85
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-08 17:24:54 +0530

    " -> 0 of 9 passed. pylint: 7.86/10 "

--- cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
+++ cspp1-practice/m9/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -1,13 +1,15 @@
+'''
 #Exercise : Function and Objects Exercise-1
 #Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+'''
 
 
 def apply_to_each(L, f):
-	for i in range(len(L)):
-		L[i] = f(L[i])
+    for i in range(len(L)):
+        L[i] = f(L[i])
 
-	print(L)
-		
+    print(L)
+        
     
 
 def main():
