fd7a35cc4f6736cad3ab0f9e460412816b585932
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-16 12:32:36 +0530

    " -> 2 of 3 passed. pylint: 9.33/10 "

--- cspp1-assignments/m14/CodeCampPoker/poker.py
+++ cspp1-assignments/m14/CodeCampPoker/poker.py
@@ -14,9 +14,9 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    
-    for i in range(0, len(hand)-1):
-        if hand[i+1]-hand[i] != 1:
+    face_values = []
+    for i in range(0, len(face_values)-1):
+        if face_values[i+1]-face_values[i] != 1:
             return False
     return True
 
