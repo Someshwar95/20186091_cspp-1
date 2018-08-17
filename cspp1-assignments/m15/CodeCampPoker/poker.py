929b9626bb9c73ba9ee07d5badc56ee051438517
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-17 19:00:10 +0530

    " -> 12 of 19 passed. pylint: 6.67/10 "

--- cspp1-assignments/m15/CodeCampPoker/poker.py
+++ cspp1-assignments/m15/CodeCampPoker/poker.py
@@ -9,6 +9,10 @@ def is_four_of_a_kind(hand):
     face_values = get_face_values(hand)
     face_values.sort()
     return len(set(face_values[:-1])) == 1 or len(set(face_values[:-4])) == 1
+def is_three_of_a_kind(hand):
+    face_values = get_face_values(hand)
+    face_values.sort()
+    return len(set(face_values)) == 3
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
