8638de642537d9ac2f6bb1f5aee68dec5241ae3c
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-17 10:15:01 +0530

    " -> 5 of 8 passed. pylint: 9.34/10 "

--- cspp1-assignments/m14/CodeCampPokerVersion2/pokerversiontwo.py
+++ cspp1-assignments/m14/CodeCampPokerVersion2/pokerversiontwo.py
@@ -19,7 +19,8 @@ def is_three_of_a_kind(hand):
     '''
     for i in range(len(hand)-2):
         if hand[i] == hand[i+1] == hand[i+2]:
-            return False
+            return True
+        return False
 
 def is_one_pair(hand):
     '''
