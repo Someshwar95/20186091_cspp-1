a0d153d2b83e386aed8f12e21091621d522a76e9
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-23 21:35:16 +0530

    " -> 1 of 8 passed. pylint: 3.73/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -43,7 +43,7 @@ def add_matrix(matrix_one, matrix_two):
     if len(matrix_one) == len(matrix_two) and len(matrix_one[0]) == len(matrix_two[0]):
         for i in range(len(matrix_one)):
             for j in range(len(matrix_one[0])):
-                add_matrix1[i][j] = (matrix_one[i][j] + matrix_two[i][j])
+                add_matrix1[i][j] += (matrix_one[i][j] + matrix_two[i][j])
         return add_matrix1
     else:
         print("Error: Matrix shapes invalid for addition")
