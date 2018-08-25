06dbc09d625761b73383cf50974ccdae4b37ca57
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-08-25 11:57:39 +0530

    " -> 0 of 26 passed. pylint: 0/0 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -13,10 +13,12 @@ def check_sudoku(sudoku):
         Your solution goes here. You may add other helper functions as needed.
         The function has to return True for a valid sudoku grid and false otherwise
     '''
-    # if sum(i.count('x') for i in tic_tac_toe) > 5 or sum(i.count('o') for i in tic_tac_toe) > 5 or \
-    # (sum(i.count('o') for i in tic_tac_toe) == sum(i.count('x') for i in tic_tac_toe)):
+     if sum(i.count('1') for i in sudoku) > 9 or sum(i.count('2') for i in sudoku) > 9 or sum(i.count('3') for i in sudoku) > 9 \
+     or sum(i.count('4') for i in sudoku) > 9 or sum(i.count('5') for i in sudoku) > 9 or sum(i.count('6') for i in sudoku) > 9 \
+     or sum(i.count('7') for i in sudoku) > 9 or sum(i.count('8') for i in sudoku) > 9 or sum(i.count('9') for i in sudoku) > 9:
+     # (sum(i.count('o') for i in sudoku) == sum(i.count('x') for i in sudoku)):
         #print("invalid game")
-        #return False
+        return False
     for i in range(len(sudoku)):
         for j in sudoku[i]:
             if j not in '123456789':
