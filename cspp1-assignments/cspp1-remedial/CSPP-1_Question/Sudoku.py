7b979910262ac713fa472997b6c3cae11cb94a3e
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-12-21 11:03:34 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -17/10 "

--- cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
+++ cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
@@ -1,5 +1,5 @@
 def print_grid(arr): 
     for i in range(9): 
         for j in range(9): 
-            print (arr[i][j])
-        print ('Given sudoku is solved') 
+            #print (arr[i][j])
+            print ('Given sudoku is solved') 
