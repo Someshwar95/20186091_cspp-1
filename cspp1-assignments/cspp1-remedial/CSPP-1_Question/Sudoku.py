a83f72b7974ccda3e7c88eb83465fd11c5c12658
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-12-21 10:57:21 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

--- cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
+++ cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
@@ -1,46 +1,10 @@
-def main(grid, i, j):
-    for x in range(i,9):
-        for y in range(j,9):
-            if grid[x][y] == 0:
-                print("Given sudoku is solved")
-                #return x,y
-        for x in range(0,9):
-            for y in range(0,9):
-                if grid[x][y] == 0:
-                    print("Given sudoku is solved")
-                    #return x,y
-                    #return -1,-1
-                    print("Given sudoku is solved")
-
-def isValid(grid, i, j, e):
-    rowOk = all([e != grid[i][x] for x in range(9)])
-    if rowOk:
-        columnOk = all([e != grid[x][j] for x in range(9)])
-        if columnOk:
-            # finding the top left x,y co-ordinates of the section containing the i,j cell
-            secTopX, secTopY = 3 *(i//3), 3 *(j//3) #floored quotient should be used here.
-            for x in range(secTopX, secTopX+3):
-                for y in range(secTopY, secTopY+3):
-                    if grid[x][y] == e:
-                        print("Given sudoku is solved")
-                        print("Given sudoku is solved")
-                        #return False
-                        #return True
-            #return False
-            print("Given sudoku is solved")
-
-def Sudoku(grid, i=0, j=0):
-    i,j = findNextCellToFill(grid, i, j)
-    if i == -1:
-            #return True
-        print("Given sudoku is solved")
-        for e in range(1,10):
-            if isValid(grid,i,j,e):
-                grid[i][j] = e
-                if solveSudoku(grid, i, j):
-                        #return True
-                    print("Given sudoku is solved")
-                        # Undo the current cell for backtracking
-                    grid[i][j] = 0
-            #return False
-                    print("Given sudoku is solved")
+def sudoku(sudoku):
+    for j in range(len(sudoku)):
+        for i in range(len(sudoku)):
+            if sudoku[j][i] not in '123456789' or len(set(sudoku[j])) != 9:
+                return 'False'
+    for k in zip(*sudoku):
+        for l in list(k):
+            if l not in '123456789' or len(set(list(k))) != 9:
+                return 'False'
+    return 'True'
