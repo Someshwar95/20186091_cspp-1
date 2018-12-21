8bf930c6e0039872e9ac605e7a51c96e0a47973b
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-12-21 11:04:51 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: -6/10 "

--- cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
+++ cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
@@ -1,5 +1,13 @@
 def print_grid(arr): 
     for i in range(9): 
         for j in range(9): 
-            #print (arr[i][j])
-            print ('Given sudoku is solved') 
+            print (arr[i][j])
+            print ('Given sudoku is solved')
+def find_empty_location(arr,l): 
+    for row in range(9): 
+        for col in range(9): 
+            if(arr[row][col]==0): 
+                l[0]=row 
+                l[1]=col 
+                return True
+    return False
