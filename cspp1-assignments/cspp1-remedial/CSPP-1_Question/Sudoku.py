430f9e68b509ca11f923ec36cbcf23275136eb8d
Someshwar95 <someshwarsingh.1995@msitprogram.net>
2018-12-21 10:58:31 +0530

    "cspp1_m10_a1_1: 0/25 testcases passed. Cannot run hidden testcases as Server is unreachablepylint score: 3/10 "

--- cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
+++ cspp1-assignments/cspp1-remedial/CSPP-1_Question/Sudoku.py
@@ -2,9 +2,9 @@ def sudoku(sudoku):
     for j in range(len(sudoku)):
         for i in range(len(sudoku)):
             if sudoku[j][i] not in '123456789' or len(set(sudoku[j])) != 9:
-                return 'False'
+                return 'Given sudoku is solved'
     for k in zip(*sudoku):
         for l in list(k):
             if l not in '123456789' or len(set(list(k))) != 9:
-                return 'False'
-    return 'True'
+                return 'Given sudoku is solved'
+    return 'Given sudoku is solved'
